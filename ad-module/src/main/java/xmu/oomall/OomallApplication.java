75b73dfcda2b73454bdd93611768ea9d7d87e441
linlianhui <linlianhui@email.com>
2019-12-18 20:37:41 +0800

    change datasource to aliyun by linlianhui

--- ad-module/src/main/java/xmu/oomall/OomallApplication.java
+++ ad-module/src/main/java/xmu/oomall/OomallApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class OomallApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OomallApplication.class, args);
     }
-
 }
