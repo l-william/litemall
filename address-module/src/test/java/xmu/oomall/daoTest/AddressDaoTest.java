f9f0ae29c7b490bc1066ba44937d7fdab403fae1
CFH <1504403484@qq.com>
2019-12-18 17:57:12 +0800

    - addressDao测试 by cfh 12/18

--- address-module/src/test/java/xmu/oomall/daoTest/AddressDaoTest.java
+++ address-module/src/test/java/xmu/oomall/daoTest/AddressDaoTest.java
@@ -5,71 +5,101 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import xmu.oomall.dao.AddressDao;
 import xmu.oomall.domain.Address;
+import xmu.oomall.domain.AddressPo;
 
 import java.util.List;
 
+/**
+ * @author XYT,CFH
+ *
+ */
 @SpringBootTest
 public class AddressDaoTest {
-    /**
-     * xyt
-     */
     @Autowired
     private AddressDao addressDao;
     @Test
     void findAddressById(){
-        Integer a=10001;
-        Address address=addressDao.findAddressById(a);
+        Integer id=10001;
+        Address address=addressDao.findAddressById(id);
         if(address==null)
-            System.out.println("not find!");
+            System.out.println("not found");
         else
-            System.out.println(address.getId());
+            System.out.println(address.toString());
     }
     @Test
     void findAddressListByUserId(){
-        Integer a=100002;
-        List<Address> address=addressDao.findAddressListByUserId(a);
-        if(address==null)
-            System.out.println("not find!");
-        else
-            System.out.println(address.size());
+        Integer userId=100002;
+        List<Address> addressList=addressDao.findAddressListByUserId(userId);
+        if(addressList.size()==0) {
+            System.out.println("not found");
+        }
+        else {
+            for (Address address :addressList) {
+                System.out.println(address.toString());
+            }
+        }
+    }
+    @Test
+    void findAddressListByUserIdAndConsignee(){
+        Integer userId=1001;
+        String name="";
+        List<Address> addressList=addressDao.findAddressListByUserIdAndConsignee(userId,name);
+        if(addressList.size()==0) {
+            System.out.println("not found");
+        }
+        else {
+            for (Address address :addressList) {
+                System.out.println(address.toString());
+            }
+        }
     }
     @Test
     void addAddress(){
-        Address address=new Address();
-        address.setAddressDetail("厦门");
-        if(addressDao.addAddress(address)==1)
+        AddressPo addressPo=new AddressPo();
+        addressPo.setBeDefault(false);
+        addressPo.setAddressDetail("厦门");
+        AddressPo retPo=addressDao.addAddress(addressPo);
+        if(retPo!=null)
         {
-            System.out.println("add success");
+            System.out.println("add succeeded");
+            System.out.println(retPo.toString());
         }
         else
         {
-            System.out.println("failed");
+            System.out.println("add failed");
         }
     }
     @Test
     void updateAddress(){
-        Address address=new Address();
-        address.setId(10001);
-        address.setAddressDetail("xiamen");
-        if(addressDao.updateAddress(address)==1)
+        Integer id=10001;
+        AddressPo addressPo=new AddressPo();
+        addressPo.setId(id);
+        addressPo.setBeDefault(true);
+        addressPo.setAddressDetail("福州");
+        AddressPo retPo=addressDao.updateAddress(addressPo);
+        if(retPo!=null)
         {
-            System.out.println("update success");
+            System.out.println("update succeeded");
+            System.out.println(retPo.toString());
         }
         else
         {
-            System.out.println("failed");
+            System.out.println("update failed");
         }
     }
     @Test
-    void deleteAddressById(){
-        if(addressDao.deleteAddressById(1)==1)
+    void deleteAddress(){
+        Integer id=1;
+        int ret=addressDao.deleteAddress(id);
+        if(ret!=0)
         {
-            System.out.println("del success");
+            System.out.println("delete succeeded");
         }
         else
         {
-            System.out.println("failed");
+            System.out.println("delete failed");
         }
     }
+
 
 }
